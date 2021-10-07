05e9fc2cd94110fff70a7a1abcb567433ded0731
Zach Patrignani <go0632@wayne.edu>
2021-10-07 15:11:18 -0400

    Fixed a route

--- main/src/server/routes/routes.js
+++ main/src/server/routes/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const outageController = require("../controllers/outageController");
 
+router.route("/").get(outageController.getAllOutages);
 router.route("/outages").get(outageController.getAllOutages);
 router.route("/outage-new").post(outageController.createNewOutage);
 
