caad59cacf614295a270e64f2cbebedcf6a5d30e
ShafiKamal2400 <go6803@wayne.edu>
2021-10-07 17:30:53 -0400

    removed default styling and elements of react template, imported Leaflet map, and created OutageMap component

--- main/src/App.js
+++ main/src/App.js
@@ -1,24 +1,18 @@
 import logo from './css/logo.svg';
 import './css/App.css';
+import OutageMap from './components/OutageMap';
 
-function App() {
+function App() { //primary page of the site
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          page is alive.
+        </h1>
       </header>
+      <OutageMap />
     </div>
+    
   );
 }
 
