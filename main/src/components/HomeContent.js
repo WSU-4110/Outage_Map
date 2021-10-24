0ec519c4421b8ba8927d7981c01556444fd14cb7
Zach Patrignani <go0632@wayne.edu>
2021-10-24 16:16:17 -0400

    Added home page styling

--- main/src/components/HomeContent.js
+++ main/src/components/HomeContent.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import '../css/HomeContent.css'
+import React from "react";
+import "../css/HomeContent.css";
 
 const HomeContent = () => {
-    return (
-        <div className="content">
-            <p className="text">
-            <h2>What do we do?</h2>
-                Outage Map provides all users with the ability to see 
-            an outage map for a variety of services that are provided. 
-            </p>
-        </div>
-    )
-}
+  return (
+    <body>
+      <div className="content">
+        <p className="text">
+          <h2>
+            <span className="orange">O</span>
+            <span>utage </span>
+            <span className="orange">M</span>
+            <span>ap</span>
+          </h2>
+          Outage Map provides all users with the ability to see an outage map
+          for a variety of services that are provided.
+        </p>
+      </div>
+    </body>
+  );
+};
 
-export default HomeContent
+export default HomeContent;
--- main/src/components/NavBar.js
+++ main/src/components/NavBar.js
@@ -1,48 +1,55 @@
-import React from 'react'
-import {Link} from "react-router-dom";
-import '../css/NavBar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/NavBar.css";
 
 class Navbar extends React.Component {
-    state = {clicked: false}
+  state = { clicked: false };
 
-    //set clicked value to change true/false everytime it's clicked 
-    handleClick = () => {
-        this.setState({ clicked: !this.state.clicked})
-    }
+  //set clicked value to change true/false everytime it's clicked
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
 
-    render(){
-        return(
-            <nav className="NavbarItems">
-                <Link className="navbartitle" to="/">
-                    <h1>
-                        Outage Map 
-                    </h1>
-                </Link>
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <Link className="navbartitle" to="/">
+          <img src={"small-logo.png"} width="70" height="70" />
+        </Link>
 
-                {/* function that will recognize user click and change menu img from 3 layer icon to 'x'. Also the naming has to 'fas fa-' according to fontawesome */}
-                <div className="menuicon" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
+        {/* function that will recognize user click and change menu img from 3 layer icon to 'x'. Also the naming has to 'fas fa-' according to fontawesome */}
+        <div className="menuicon" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
 
-                {/* className changes based on user-click on menu icon, this is for mobile view css */}
-                <ul className={this.state.clicked ? 'navmenu active' : 'navmenu'}>
-                    <li>
-                        <Link className="navlinks" to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link className="navlinks" to='/outages'>View Outages</Link>
-                    </li>
-                    <li>
-                        <a className="navlinks" href='#'>Report Outage</a>
-                    </li>
-                    <li>
-                        <Link className="navlinks" to='/signup'>Sign Up</Link>
-                    </li>
-                </ul>
-
-            </nav>
-        )
-    }
+        {/* className changes based on user-click on menu icon, this is for mobile view css */}
+        <ul className={this.state.clicked ? "navmenu active" : "navmenu"}>
+          <li>
+            <Link className="navlinks" to="/login">
+              Log In
+            </Link>
+          </li>
+          <li>
+            <Link className="navlinks" to="/outages">
+              View Outages
+            </Link>
+          </li>
+          <li>
+            <a className="navlinks" href="#">
+              Report Outage
+            </a>
+          </li>
+          <li>
+            <Link className="navlinks" to="/signup">
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
