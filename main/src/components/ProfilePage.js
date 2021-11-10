592cbbd0a08a22aa8fe480247336ca98614c8218
wixian <wixsonian@gmail.com>
2021-11-09 23:25:45 -0500

    Addition of profile data

--- main/src/components/ProfilePage.js
+++ main/src/components/ProfilePage.js
@@ -1,14 +1,44 @@
 //profile page, with user reported outages and closed outages
 import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../components/userLogin";
+
 
 function UserProfile(props){
-    return(
-        <div className="Profile">
-            <h1 className="UserName">{this.props.email}</h1>
-            <h2 className="Password">{this.props.pwd}</h2>
-        </div>
+    const userID = useSelector(state => state.userID);
+
+    //user profile variables
+    const [profile, setProfile] = useState('')
+    const userProf = props.match.params.userProf
+
+    const userVar={userProf:userProf}
+
+    useEffect(()=>{
+        axios.post(/*'api/users/getProfile'*/, userVar)//will need to pull from database
+        .then(response=>{
+            if(response.data.success){
+                console.log(response.data)
+                setProfile(response.data.userID)            
+            } else {
+                alert('Failed to get user info')
+            }
+        })
+    }
 
     )
-}
+    return(
+        <div>
+            <h1>Profile</h1>
+            <h2>Email: </h2>
+            <h3>Password: </h3>
+        </div>
+    )
+    function UserReports{
+
+    }
+    function UserClosedReports{
+    
+    }
+};
+
+export default UserProfile;
