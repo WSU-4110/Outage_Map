bdb5fc90ab7d4ea97ed28897db84c989bf575beb
unknown <go6803@wayne.edu>
2021-11-14 22:31:34 -0500

    added functionality where different icon indicators show for each kinda of outage

--- main/src/components/OutageMap.js
+++ main/src/components/OutageMap.js
@@ -1,8 +1,16 @@
 import { MapContainer, TileLayer, Marker, Popup, Circle } from "react-leaflet";
 import React, { useState, useEffect } from "react";
+import L from "leaflet";
 import Modal from "react-modal";
 import axios from "axios";
 import ReportOutage from "./ReportOutage";
+import gaming from "./icons/gamepad-solid.svg"
+import streaming from "./icons/video-slash-solid.svg"
+import power from "./icons/plug-solid.svg"
+import internet from "./icons/wifi-solid.svg"
+import cable from "./icons/ethernet-solid.svg"
+import website from "./icons/laptop-code-solid.svg"
+import exclamation from "./icons/exclamation-solid.svg"
 
 function OutageIndicator({ outage }) {
   //this component renders the markers with corresponding lat and long values calculated by the geocodify api.
@@ -10,10 +18,62 @@ function OutageIndicator({ outage }) {
   //localStorage.clear();
   //console.log(JSON.parse(localStorage.getItem("user")));
 
+  const LeafIcon = L.Icon.extend({
+    options: {
+        iconSize:     [38, 35],
+    }
+  });
+
+  const streamingIcon = new LeafIcon({
+    iconUrl: streaming
+  });
+  const powerIcon = new LeafIcon({
+    iconUrl: power
+  });
+  const internetIcon = new LeafIcon({
+    iconUrl: internet
+  });
+  const gamingIcon = new LeafIcon({
+    iconUrl: gaming
+  });
+  const exclamationIcon = new LeafIcon({
+    iconUrl: exclamation
+  });
+  const cableIcon = new LeafIcon({
+    iconUrl: cable
+  });
+  const websiteIcon = new LeafIcon({
+    iconUrl: website
+  });
+
+  const [icon, setIcon] = useState(powerIcon);
+
   useEffect(() => {
     function resolveLocation() {
       let lng = Number(outage.longitude);
       let lat = Number(outage.latitude);
+      switch(outage.service_type) {
+        case "Streaming":
+          setIcon(streamingIcon)
+          break;
+        case "Power":
+          setIcon(powerIcon)
+          break;
+        case "Internet":
+          setIcon(internetIcon)
+          break;
+        case "Gaming":
+          setIcon(gamingIcon)
+          break;
+        case "Cable":
+          setIcon(cableIcon)
+          break;
+        case "Website":
+          setIcon(websiteIcon)
+          break;
+        default:
+          setIcon(exclamationIcon)
+      }
       setCoords({ lng, lat });
     }
     resolveLocation();
@@ -22,7 +82,7 @@ function OutageIndicator({ outage }) {
   return !coords ? (
     "Loading"
   ) : (
-    <Marker position={[coords.lat, coords.lng]}>
+    <Marker position={[coords.lat, coords.lng]} icon={icon}>
       <Popup className={outage.service_type}>
         {outage.service_type}: {outage.service_name}
       </Popup>
