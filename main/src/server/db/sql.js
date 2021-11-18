eb157326c2afb02754798762ffceb9923ed43a71
Zach Patrignani <go0632@wayne.edu>
2021-11-18 11:16:43 -0500

    Singleton Design Pattern for Database Connection

--- main/src/server/db/sql.js
+++ main/src/server/db/sql.js
@@ -1,12 +1,33 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createPool({
-  connectionLimit: 10,
-  password: "password",
-  user: "root",
-  database: "outageMapV2",
-  host: "localhost",
-  port: "3306",
-});
+//Implementing Singleton Design Pattern for database connection
 
-module.exports = connection.promise();
+class dbConnectionSingleton{
+  
+  //The '#' prefix means private, according to MDN Web Docs
+  static #CONNECTION;
+  
+  //Private constructor
+  #constructor() {
+  this.#CONNECTION = mysql.createPool({
+    connectionLimit: 10,
+    password: "password",
+    user: "root",
+    database: "outageMapV2",
+    host: "localhost",
+    port: "3306",
+});
+   }
+
+  static getConnection() {
+    if (this.#CONNECTION == null)
+      this.#CONNECTION = new dbConnectionSingleton()
+    return this.#CONNECTION
+    
+
+  }
+
+}
+
+
+module.exports = dbConnectionSingleton;
