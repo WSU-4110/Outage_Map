e57c4cdc031ba443b4f752452a1ff836cb8e3b96
wixian <wixsonian@gmail.com>
2021-12-09 07:45:21 -0500

    ProfilePage with data passed, needs to sort by status and testing

--- main/src/components/ProfilePage.js
+++ main/src/components/ProfilePage.js
@@ -1,6 +1,6 @@
 //profile page, with user reported outages and closed outages
 import React from "react";
-import { useHistory, withRouter } from "react-router-dom";
+import { useHistory, useParams, withRouter } from "react-router-dom";
 import { useState, useEffect } from "react";
 import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
 import { TabContent } from 'react-bootstrap'
@@ -8,24 +8,87 @@ import axios from "axios";
 import "../css/profile.css";
 
 function Profile({outage}) {
-  //set tabs states
-  const [selectTab, setSelectTab] = useState("reported");   
-  
+  //set loggedInUser from localstorage
   const [loggedInUser, setLoggedInUser] = useState(JSON.parse(localStorage.getItem("user")));
-   
   //set table outages
   const [tableOutage, setTableOutage] = useState([]);
-
-  function handleChange(event, newSelect) {
-    setSelectTab(newSelect);
+  const tabData = {
+    "reported": {
+      title: "Open Reported Outages",
+      content: (         
+             <table class="table">
+               <thead class="thead">
+                 <tr>
+                 <th scope="col">Service Type</th>
+                <th scope="col">Service Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Status</th>
+                <th scope="col">Date Created</th>
+                 </tr>
+               </thead>
+               <tbody>
+                 {tableOutage.map(item =>  {
+                   return <tr key={item.Id}>
+                     <td>{item.service_type}</td>
+                     <td>{item.service_name}</td>
+                     <td>{item.outage_description}</td>
+                     <td>{item.outage_status}</td>
+                     <td>{item.date_created}</td>
+                   </tr>;
+                 })}
+               </tbody>
+             </table>         
+        )    
+    },
+  "closed": {
+    title: "Closed Reported Outages",
+    content: (      
+          <table class="table">
+            <thead class="thead">
+              <tr>
+                <th scope="col">Service Type</th>
+                <th scope="col">Service Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Status</th>
+                <th scope="col">Date Created</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableOutage.map(item => {
+                return <tr key={item.Id}>
+                  <td>{item.service_type}</td>
+                  <td>{item.service_name}</td>
+                  <td>{item.outage_description}</td>
+                  <td>{item.outage_status}</td>
+                  <td>{item.date_created}</td>
+                </tr>;
+              })}
+            </tbody>
+          </table>        
+      )    
   }
-   //let history = useHistory();
-   
+}
+  //set tabs states
+  const [selectTab, setSelectTab] = useState("reported");   
+  //toggle between tabs
+  
+  const history = useHistory();
    useEffect(() => {
+    if(!selectTab){
+        history.push(`reported`);
+    }
+  }, [history, selectTab]);
+  function handleTab(newSelect) {
+    if (selectTab !== newSelect)
+      history.push(`/${newSelect}`);
+  }
+   
+  
+  useEffect(() => {
     async function fetchOutages() {
       const resp = await axios.get("/outages");
       setTableOutage(resp.data.outages);
-    }
+  }
     fetchOutages();
   }, []);
    
@@ -36,27 +99,23 @@ function Profile({outage}) {
             <p className="m-1 mx-4 w-auto" style={{ color: 'orange' }}>Username: {loggedInUser}</p>
 
         <Tabs //tabbed data
-          value = {selectTab} 
-          onChange={handleChange}
+          tabData 
+          onChange={handleTab}
           className="mx-1 w-100"
           centered="true">
-        
-        <TabList> 
-          <Tab eventKey="reported" className="profiletab">Reported Outages</Tab>
-          <Tab eventKey="closed" className="profiletab">Closed Outages</Tab>          
-        </TabList>
-
-        <TabPanel className="tabscontent">
-            <TabContent style={{color:'white'}}>Reported: {tableOutage.map((val)=>{
-              return <p>Service Name: {val.outage.service_name} | Outage Description: {val.outage.outage_description}</p>
-             })}</TabContent>            
-        </TabPanel>
-        <TabPanel className="tabscontent">
-            <TabContent style={{color:'white'}}>Closed: {tableOutage.map((val)=>{
-              return <p>Service Name: {val.outage.service_name} | Outage Description: {val.outage.outage_description}</p>
-             })}</TabContent>            
-        </TabPanel>
-      </Tabs>
+          
+          <TabList>
+            <Tab className="profiletab" {...selectTab === tabData["reported"]? "active" : ""} onChange={()=>{handleTab(tabData["reported"]);}}>{tabData["reported"].title}</Tab> 
+            <Tab className="profiletab" {...selectTab === tabData["closed"]? "active" : ""} onChange={()=>{handleTab(tabData["closed"]);}}>{tabData["closed"].title}</Tab>          
+          </TabList>
+          <TabPanel className="tabscontent" >
+            <TabContent style={{color:'white'}}>Reported: {tabData["reported"].content}</TabContent>            
+          </TabPanel>
+          <TabPanel className="tabscontent">
+            <TabContent style={{color:'white'}}>Closed: {tabData["closed"].content}</TabContent>            
+          </TabPanel> 
+        </Tabs>         
+      
         </div>
     )
 }
